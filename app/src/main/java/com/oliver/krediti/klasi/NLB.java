e083f03d1cec97b3bf7597e640aa7c1c7c2b92b5
oliverMKD <oliverdamjan2013@gmail.com>
2018-04-23 18:36:05 +0200

    prroba

---
+++ app/src/main/java/com/oliver/krediti/klasi/NLB.java
@@ -0,0 +1,60 @@
+package com.oliver.krediti.klasi;
+
+import java.io.Serializable;
+
+public class NLB implements Serializable {
+
+    String auto;
+    String depozit;
+    String hipotek;
+    String potr;
+    String stanben;
+
+    public NLB(String auto, String depozit, String hipotek, String potr, String stanben) {
+        this.auto = auto;
+        this.depozit = depozit;
+        this.hipotek = hipotek;
+        this.potr = potr;
+        this.stanben = stanben;
+    }
+
+    public String getAuto() {
+        return auto;
+    }
+
+    public void setAuto(String auto) {
+        this.auto = auto;
+    }
+
+    public String getDepozit() {
+        return depozit;
+    }
+
+    public void setDepozit(String depozit) {
+        this.depozit = depozit;
+    }
+
+    public String getHipotek() {
+        return hipotek;
+    }
+
+    public void setHipotek(String hipotek) {
+        this.hipotek = hipotek;
+    }
+
+    public String getPotr() {
+        return potr;
+    }
+
+    public void setPotr(String potr) {
+        this.potr = potr;
+    }
+
+    public String getStanben() {
+        return stanben;
+    }
+
+    public void setStanben(String stanben) {
+        this.stanben = stanben;
+    }
+}
---
+++ app/src/main/java/com/oliver/krediti/model/NLB_model.java
@@ -0,0 +1,10 @@
+package com.oliver.krediti.model;
+
+import com.oliver.krediti.klasi.NLB;
+
+import java.util.ArrayList;
+
+public class NLB_model {
+
+    ArrayList<NLB> nlb = new ArrayList<>();
+}
